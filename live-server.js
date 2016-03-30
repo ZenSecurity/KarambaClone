06f29b6b769bea23e9949cab00633d48cd8385ea
Pavel Zhytko <pjitco@gmail.com>
2016-03-30 04:25:06 +0300

    about-us page

--- live-server.js
+++ live-server.js
@@ -6,7 +6,7 @@ var params = {
     root: "./client", // Set root directory that's being server. Defaults to cwd.
     //open: false, // When false, it won't load your browser by default.
     //ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-    file: "approach.html", // When set, serve this file for every 404 (useful for single-page applications)
+    file: "about.html", // When set, serve this file for every 404 (useful for single-page applications)
     //wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
     mount: [['/lib', './node_modules']] // Mount a directory to a route.
 };
