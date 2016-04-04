5f38be2b69c8ef1c6ec1399c52889dc7e69a9727
Pavel Zhytko <pjitco@gmail.com>
2016-04-04 21:17:49 +0300

    new pawd

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ var auth = require('http-auth');
 
 var PORT = Number(process.env.PORT) || 8080;
 var USERNAME = 'KarambaSecurityPress';
-var PASSWORD = 'Embargo-April-7-900am-EDT';
+var PASSWORD = 'UnderEmbargo';
 
 var basicAuth = auth.basic({ realm: "Karamba"}, function (username, password, callback) {
         callback(username === USERNAME && password === PASSWORD);
